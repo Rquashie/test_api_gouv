aeda6aba42e53b53744d3e433169f774843d3711
rQuashie <kangoojuniors93@gmail.com>
2025-01-31 19:20:30 +0100

    feat : test API

--- traitement_utilisateur.php
+++ traitement_utilisateur.php
@@ -1,14 +1,32 @@
 <?php
 
+
+
 $numeroRue = $_POST['numeroRue'];
 $nomRue = $_POST['nomRue'];
 $ville = $_POST['ville'];
 $cp = $_POST['cp'];
 
 $url_base = "https://api-adresse.data.gouv.fr/search/?q=" ;
-$q =$numeroRue.$nomRue.$ville."&postcode=".$cp;
+$adresse = $numeroRue ."".$nomRue ;
+$q =urlencode($numeroRue.$nomRue.$ville.$cp);
 $url_complete = $url_base . $q;
-echo "<h2> $url_complete </h2>";
+$reponse = file_get_contents($url_complete);
+$data = json_decode($reponse, true);
+if(!empty($data['features'])) {
+
+    $resultat = $data['features'][0]['properties'];
+    echo "<h3>Adresse valide</h3>";
+}
+else {
+    echo "<h3>Adresse invalide</h3>";
+}
+//CREE un Autocomplete HTML avec l'API et verifier l'adresses
+
+
+
+
 ?>
+
 
 
