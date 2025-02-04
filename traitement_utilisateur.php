63494b5d39e32657dc46a6e4f6f2826d548fb428
rQuashie <kangoojuniors93@gmail.com>
2025-02-04 06:16:50 +0100

    feat : test api

--- traitement_utilisateur.php
+++ traitement_utilisateur.php
@@ -8,22 +8,31 @@ $ville = $_POST['ville'];
 $cp = $_POST['cp'];
 
 $url_base = "https://api-adresse.data.gouv.fr/search/?q=" ;
-$adresse = $numeroRue ."".$nomRue ;
-$q =urlencode($numeroRue.$nomRue.$ville.$cp);
-$url_complete = $url_base . $q;
-$reponse = file_get_contents($url_complete);
-$data = json_decode($reponse, true);
-if(!empty($data['features'])) {
-
-    $resultat = $data['features'][0]['properties'];
-    echo "<h3>Adresse valide</h3>";
+$adresse_complete = $numeroRue." ".$nomRue." ".$cp." ".$ville; ; ;
+$q =urlencode("$numeroRue $nomRue $ville");
+$url_requete = $url_base . $q;
+$reponse = file_get_contents($url_requete);
+if($reponse == false){
+    echo "<h3> Adresse invalide </h3>" ;
 }
 else {
-    echo "<h3>Adresse invalide</h3>";
+
+    $data = json_decode($reponse, true);
+    $resultat = $data['features'][0]['properties'];
+
+    $ville_api = $resultat['city'];
+    $adresse_api = $resultat['label'];
+
+
+    $similaire_ville = similar_text($adresse_complete, $adresse_api, $pourcentage);
+    if ($pourcentage > 92) {
+        echo "<h3> $url_requete </h3>";
+        echo "<h3> Adresse trouvée : $adresse_api </h3>";
+
+    } else {
+        echo "<h3> Adresse introuvable </h3>";
+    }
 }
-//CREE un Autocomplete HTML avec l'API et verifier l'adresses
-
-
 
 
 ?>
